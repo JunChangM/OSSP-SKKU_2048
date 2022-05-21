a4f76161883bed054176e7f4cf683140d61ee175
JunChangM <jsjs1209@g.skku.edu>
2022-05-21 20:24:50 +0900

    loginstate

--- logic.js
+++ logic.js
@@ -1,29 +1,33 @@
-/*login.html*/
+/*index.html*/
 
 const id = document.getElementById('id')
 const password = document.getElementById('password')
 const login = document.getElementById('login')
 let errStack = 0;
 
+function loginlocalstorage(loginid){
+
+}
 
 login.addEventListener('click', () => {
     lastid = id.value;
-    
     const lastpassword = localStorage.getItem(lastid);
 
     if (id.value == 'user') {
         if (password.value == '0000') {
             alert('테스트용 계정으로 로그인 되었습니다!');
-            window.location.replace("index.html");
+            window.location.replace("main.html");
         }
         else {
             alert('비밀번호를 다시 입력해주세요.');
             errStack ++;
         }
     }
-    else if (lastpassword == id.value) {
+    else if (lastpassword == password.value) {
         alert(`${id.value}님 로그인 되었습니다. 재방문을 환영합니다.`);
-        window.location.replace("index.html");
+        localStorage.removeItem("user");
+        localStorage.setItem("user",id.value);
+        window.location.replace("main.html");
 
     }
     else {
@@ -31,7 +35,9 @@ login.addEventListener('click', () => {
         textid = id.value; 
         textpw = password.value; 
         localStorage.setItem(textid, textpw);
-        window.location.replace("index.html");
+        localStorage.removeItem("user");
+        localStorage.setItem("user",textid);
+        window.location.replace("main.html");
     }
 
     if (errStack >= 5) {
